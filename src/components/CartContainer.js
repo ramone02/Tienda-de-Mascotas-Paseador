1258c8e651f25e9ebbc548a5ac312f255033f2d2
Leonardo Bellido <leobellido@gmail.com>
2022-09-11 13:36:48 -0300

    aplico logica, onAdd(no duplicado) remove() & cleanCart

--- src/components/CartContainer.js
+++ src/components/CartContainer.js
@@ -3,14 +3,14 @@ import { CartContext } from "../context/CartContext";
 
 const CartContainer = () => {
     const [loading, setLoading] = useState(null);
-    const { productCartList } = useContext(CartContext);
+    const { productCartList, addProduct, removeProduct, cleanCart } = useContext(CartContext);
     console.log(`productos lista ${productCartList.map((e) => e.nombre)}`);
 
     
     return(
         <>
         <h1>Cart Container</h1>
-            {productCartList && productCartList.map((e)=> <p>{e.nombre} - {e.precio}</p>)}
+            {productCartList && productCartList.map((e)=> <p key={e.id}>{e.nombre} - {e.precio} - {e.cantidad}</p>)}
         </>
     );
 }
--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,20 +1,49 @@
-import {createContext, useState} from 'react';
+import { createContext, useState } from 'react';
 
 export const CartContext = createContext();
 
-export const CartProvider = ({children})=> {
+export const CartProvider = ({ children }) => {
 
     const [productCartList, setProductCartList] = useState([]);
 
-    const addProduct = (product)=>{
-        const newProducts = [...productCartList, product];
-        console.log(newProducts.filter((e)=> e.nombre));
-        setProductCartList(newProducts);
+    //si existe obtengo el indice
+    const exists = (id) => {
+        return productCartList.findIndex((producto) => producto.id === id);
     }
 
-    return(
-        <CartContext.Provider value={{ productCartList, addProduct}}>
-            {/*...Components*/ }
+    const addProduct = (product) => {
+        //Si producto existe en el carrito le sumo la cantidad que venga en el obj "producto"
+        //si no lo agrego directamente al carrito
+        const indexProduct = exists(product.id);
+        if (indexProduct !== -1) {
+            const copyProducts = [...productCartList];
+            copyProducts[indexProduct].cantidad += product.cantidad;
+            setProductCartList(copyProducts);
+        } else {
+            const newProducts = [...productCartList, product];
+            setProductCartList(newProducts);
+        }
+    }
+
+    const removeProduct = (id) => {
+        const indexProduct = exists(id);
+        const copyProducts = [...productCartList];
+        if (indexProduct !== -1 && copyProducts[indexProduct].cantidad > 1) {
+            copyProducts[indexProduct].cantidad -= 1;
+            setProductCartList(copyProducts);
+        } else {
+            copyProducts.splice(indexProduct, 1);
+            setProductCartList(copyProducts);
+        }
+    }
+
+    const cleanCart = () => {
+        productCartList?.length > 0 ? setProductCartList([]) : setProductCartList(productCartList);
+    }
+
+    return (
+        <CartContext.Provider value={{ productCartList, addProduct, removeProduct, cleanCart }}>
+            {/*...Components*/}
             {children}
         </CartContext.Provider>
     );
